3f7f05e23228ef546593968f068c0f5280a76429
Dora Noda <dev@stargately.com>
2023-04-26 02:33:33 -0700

    uplift sliding logos

--- src/components/sliding-logos.tsx
+++ src/components/sliding-logos.tsx
@@ -1,63 +1,73 @@
-import React from "react"
+import React from "react";
 import styles from "./sliding-logos.module.css";
 
 export const SlidingLogos = () => {
   // https://jsfiddle.net/sergdenisov/wb28eeh2/3/
   return (
-    <section>
+    <div>
       <div className={styles.container}>
+        <div className="row text-center m-2">
+          <div className="col">
+            <small className="text-uppercase">
+              Built or trusted by teams from
+            </small>
+          </div>
+        </div>
         <div className="photobanner">
-          <img className="photobanner-first" src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/mysten-logo.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/secure3.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png"/>
+          <img
+            className="photobanner-first"
+            src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png"
+          />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/mysten-logo.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/secure3.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png" />
 
-          <img src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/mysten-logo.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/secure3.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png"/>
+          <img src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/mysten-logo.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/secure3.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png" />
 
-          <img src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/mysten-logo.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/secure3.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png"/>
+          <img src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/mysten-logo.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/secure3.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png" />
 
-          <img src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg"/>
-          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png"/>
+          <img src="https://tp-misc.b-cdn.net/blockeden/Google_2015_logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-global-1.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/NationalUniversityofSingapore.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Meta_Platforms_Inc._logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/microsoft.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1600px-Uber_logo_2018.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/1000px-Yale_University_logo.svg.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/iotex-logo.png" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/aptos-logo.svg" />
+          <img src="https://tp-misc.b-cdn.net/blockeden/Harvard_University_logo.svg.png" />
         </div>
       </div>
-    </section>
-  )
-}
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-import React, {useEffect, useRef} from 'react';
-import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import React, { useEffect, useRef } from "react";
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 import Hls from "hls.js";
-import styles from './index.module.css';
-import {GetStarted} from "@site/src/components/get-started";
-import {News} from "@site/src/components/news";
-import {Features} from "@site/src/components/features";
-import { beConfig } from '../config';
-import {useAos} from "@site/src/components/use-aos";
-import {SeoHead} from "@site/src/components/seo-head";
-import {SlidingLogos} from "@site/src/components/sliding-logos";
+import styles from "./index.module.css";
+import { GetStarted } from "@site/src/components/get-started";
+import { News } from "@site/src/components/news";
+import { Features } from "@site/src/components/features";
+import { beConfig } from "../config";
+import { useAos } from "@site/src/components/use-aos";
+import { SeoHead } from "@site/src/components/seo-head";
+import { SlidingLogos } from "@site/src/components/sliding-logos";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   const videoRef = useRef<HTMLVideoElement>();
   const sourceRef = useRef<HTMLSourceElement>();
   useAos();
@@ -27,20 +27,23 @@ function HomepageHeader() {
       hls.loadSource(videoSrc);
       hls.attachMedia(videoRef?.current);
     }
-      // HLS.js is not supported on platforms that do not have Media Source
-      // Extensions (MSE) enabled.
-      //
-      // When the browser has built-in HLS support (check using `canPlayType`),
-      // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-      // element through the `src` property. This is using the built-in support
-      // of the plain video element, without using HLS.js.
-      //
-      // Note: it would be more normal to wait on the 'canplay' event below however
-      // on Safari (where you are most likely to find built-in HLS support) the
-      // video.src URL must be on the user-driven white-list before a 'canplay'
-      // event will be emitted; the last video event that can be reliably
+    // HLS.js is not supported on platforms that do not have Media Source
+    // Extensions (MSE) enabled.
+    //
+    // When the browser has built-in HLS support (check using `canPlayType`),
+    // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
+    // element through the `src` property. This is using the built-in support
+    // of the plain video element, without using HLS.js.
+    //
+    // Note: it would be more normal to wait on the 'canplay' event below however
+    // on Safari (where you are most likely to find built-in HLS support) the
+    // video.src URL must be on the user-driven white-list before a 'canplay'
+    // event will be emitted; the last video event that can be reliably
     // listened-for when the URL is not on the white-list is 'loadedmetadata'.
-    else if (videoRef.current && videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
+    else if (
+      videoRef.current &&
+      videoRef.current.canPlayType("application/vnd.apple.mpegurl")
+    ) {
       sourceRef.current.type = "application/x-mpegURL";
       videoRef.current.src = videoSrc;
       videoRef.current.setAttribute("playsinline", "true");
@@ -48,7 +51,7 @@ function HomepageHeader() {
   }, []);
 
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <video
         className={styles.videoStream}
         autoPlay
@@ -58,16 +61,14 @@ function HomepageHeader() {
         poster="https://customer-wmy0lgubd5pjy3fx.cloudflarestream.com/ff109bde61cd702f3c42aeae450244b4/thumbnails/thumbnail.jpg"
         ref={videoRef}
       >
-        <source ref={sourceRef}/>
+        <source ref={sourceRef} />
       </video>
       <div className={clsx("container", styles.heroContainer)}>
         <div className="row align-items-center min-vh-50">
           <div className="col-lg-6 text-center text-lg-left mb-lg-0 m-4">
             <h1 className="hero__title">{siteConfig.tagline}</h1>
             <div className="my-4">
-              <p className="lead">
-                {beConfig.description}
-              </p>
+              <p className="lead">{beConfig.description}</p>
             </div>
 
             <a
@@ -84,19 +85,17 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={beConfig.title}
-      description={siteConfig.tagline}>
-      <SeoHead/>
+    <Layout title={beConfig.title} description={siteConfig.tagline}>
+      <SeoHead />
 
-      <HomepageHeader/>
+      <HomepageHeader />
       <main>
-        <Features/>
-        <News/>
-        <SlidingLogos/>
-        <GetStarted/>
+        <SlidingLogos />
+        <Features />
+        <News />
+        <GetStarted />
       </main>
     </Layout>
   );
