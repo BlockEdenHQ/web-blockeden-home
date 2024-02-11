901d9f6c6c880a174197ba12c505be78da285425
Tian Pan <puncsky@gmail.com>
2024-02-11 05:08:40 -0800

    update pricing

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -130,7 +130,7 @@ export default function Pricing() {
                         <span>
                           <Link
                             href={
-                              "https://blockeden.xyz/dash/create-payment-subscription?planId=price_8L39qapm4QmesQC4yGNV&isCrypto=true"
+                              "https://blockeden.xyz/dash/create-payment-subscription?planId=price_8L39qapm4QmesQC4yGNV&isCrypto=true&isPayton=true"
                             }
                           >
                             Pay with crypto
@@ -181,7 +181,7 @@ export default function Pricing() {
                         <span>
                           <Link
                             href={
-                              "https://blockeden.xyz/dash/create-payment-subscription?planId=price_AkhvFdSckLtLdnMZyhCM&isCrypto=true"
+                              "https://blockeden.xyz/dash/create-payment-subscription?planId=price_AkhvFdSckLtLdnMZyhCM&isCrypto=true&isPayton=true"
                             }
                           >
                             Pay with crypto
@@ -231,7 +231,7 @@ export default function Pricing() {
                         <span>
                           <Link
                             href={
-                              "https://blockeden.xyz/dash/create-payment-subscription?planId=price_s8G1uobrd9FUwqi7wc3N&isCrypto=true"
+                              "https://blockeden.xyz/dash/create-payment-subscription?planId=price_s8G1uobrd9FUwqi7wc3N&isCrypto=true&isPayton=true"
                             }
                           >
                             Pay with crypto
