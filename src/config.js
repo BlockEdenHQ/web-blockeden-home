173a76e0c119649573d207cac7e99ae0eb6e1403
Tian Pan <puncsky@gmail.com>
2024-03-11 16:29:28 -0700

    update

--- src/config.js
+++ src/config.js
@@ -12,6 +12,6 @@ module.exports = {
       "https://tp-misc.b-cdn.net/blockeden/BlockEden-xyz-image.png",
     title: `BlockEden.xyz - Soroban Indexer, Aptos Indexer and RPCs for Soroban, Sui, Aptos, Solana, and 12 EVM blockchains`,
     description:
-      "BlockEden.xyz is an API marketplace and staking node infra powering DApps of all sizes. Build DApp and scale faster.",
+      "BlockEden.xyz is an API marketplace and staking node infra powering DApps of all sizes, with $50M staked, 27 APIs , 6000+ developers and 99.9% overall uptime. Build DApp and scale faster.",
   },
 };
